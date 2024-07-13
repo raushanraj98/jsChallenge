d8e9e5ab4fa5df47a3b3b7f686c095a28880b056
Raushan Raj <roushancri@gmail.com>
2024-07-13 21:51:24 +0530

    feat: challenge day 1 completed

---
+++ variables/variable.js
@@ -0,0 +1,46 @@
+// Activity 1
+// Task 1
+var a = 1;
+console.log(a);
+
+// Task 2
+let name = 'JavaScript';
+console.log(name);
+
+
+// Activity 2
+// Task 3
+
+const isLoggedIn = true;
+console.log(isLoggedIn);
+
+// Activity 3
+// Task 4
+let num = 10;
+let str = 'Hello';
+let bool = true;
+let arr = [1,2,3,4,5];
+let obj ={
+  name: 'javascript',
+  age: 25
+}
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof obj);
+
+// Activity 4
+// Task 5
+
+let x = 100;
+console.log(x);
+x = 'Hello';
+console.log(x);
+
+// Activity 5
+// Task 6
+
+const er = 'Hello error';
+er = 'This is a new error';
+console.log(er);
