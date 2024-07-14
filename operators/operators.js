4b34faa2fb92f3a54c5b0fb1edb8c4e86501b68a
Raushan Raj <roushancri@gmail.com>
2024-07-14 13:47:44 +0530

    feat: Day 2 challenge Completed

---
+++ operators/operators.js
@@ -0,0 +1,44 @@
+let num1 = 10;
+let num2 = 20;
+
+let sum = num1 + num2;
+console.log(sum);
+
+let sub = num1 - num2;
+console.log(sub);
+
+let mul = num1 * num2;
+console.log(mul);
+
+let div = num1 / num2;
+console.log(div);
+
+let rem = num1 % num2;
+console.log(rem);
+
+num2 += num1;
+console.log(num2);
+
+num2 -= num1;
+console.log(num2);
+
+console.log(num1 >= num2)
+console.log(num1 > num2)
+console.log(num1 < num2)
+console.log(num1 <= num2)
+
+let num2s = '20';
+
+console.log(num2 == num2s)
+console.log(num2 === num2s)
+
+let cond1 = true;
+let cond2 = false;
+
+console.log(cond1 && cond2);
+console.log(cond1 || cond2);
+console.log(!cond1);
+
+let res = num1 > 0 ? '+ve' : '-ve';
+
+console.log(res)
